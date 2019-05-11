f26166a0da98397ba210ec7096214951aebff471
Zebiano <sebas1@hotmail.de>
2019-05-11 18:32:13 +0100

    User registration is now a thing.

--- app/src/main/java/com/sihbar/netz/Launch.java
+++ app/src/main/java/com/sihbar/netz/Launch.java
@@ -5,17 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Launch extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_launch);
     }
 
-    public void openLaunchActivity(View view)
+    public void openRegisterActivity(View view)
     {
-        Intent intent = new Intent(this, Launch.class);
+        Intent intent = new Intent(this, Register.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/sihbar/netz/Register.java
@@ -0,0 +1,90 @@
+package com.sihbar.netz;
+
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Register extends AppCompatActivity {
+
+    // Variables
+    private Button buttonRegister;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private ProgressDialog progressDialog;
+
+    // Firebase
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        // Firebase
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        // ProgressBar
+        progressDialog = new ProgressDialog(this);
+
+        // Buttons
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+
+        // EditTexts
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+    }
+
+    // OnClick of ButtonRegister
+    public void clickButtonRegister(View view) {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        LogMe(email);
+        LogMe(password);
+
+        // If input is empty
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please entner a valid email and password", Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            progressDialog.setMessage("Registering User");
+            progressDialog.show();
+
+            LogMe("Ca estamos");
+
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            LogMe( "Completed");
+                            if (task.isSuccessful()) {
+                                LogMe( "Successo!");
+                                progressDialog.cancel();
+                                Toast.makeText(Register.this, "Successfully registered User!", Toast.LENGTH_SHORT).show();
+                            } else {
+                                LogMe( "Falhaco");
+                                progressDialog.cancel();
+                                Toast.makeText(Register.this, "Failed registering User!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+    // Logs to console. ONLY STRINGS
+    private void LogMe(String input) {
+        Log.d("ActivityRegister", input);
+    }
+}
