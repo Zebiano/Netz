2269ecae6719c4814d21e74e65d140f3f28abb07
Zebiano <sebas1@hotmail.de>
2019-05-26 20:39:39 +0100

    - Add: Users now get saved onto the FireStore database

--- app/src/main/java/com/sihbar/netz/Register.java
+++ app/src/main/java/com/sihbar/netz/Register.java
@@ -12,20 +12,30 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Register extends AppCompatActivity {
 
     // Variables
-    private String TAG = "ActivityRegister";
-    private EditText editTextEmail;
+    private static final String TAG = "Register";
+    private EditText editTextName;
+    private EditText editTextPhone;
     private EditText editTextPassword;
+    private EditText editTextPasswordR;
+    private EditText editTextEmail;
+    private EditText editTextCountry;
     private ProgressDialog progressDialog;
 
     // Firebase
     private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,30 +44,47 @@ public class Register extends AppCompatActivity {
 
         // Firebase
         firebaseAuth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
 
         // ProgressBar
         progressDialog = new ProgressDialog(this);
 
         // EditTexts
-        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextPhone = (EditText) findViewById(R.id.editTextPhone);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        editTextPasswordR = (EditText) findViewById(R.id.editTextPasswordR);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextCountry = (EditText) findViewById(R.id.editTextCountry);
     }
 
     // OnClick of ButtonRegister
     public void clickButtonRegister(View view) {
-        String email = editTextEmail.getText().toString().trim();
-        String password = editTextPassword.getText().toString().trim();
-        //Log.d(TAG, email);
+        final String name = editTextName.getText().toString().trim();
+        final String phone = editTextPhone.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
+        final String passwordR = editTextPasswordR.getText().toString().trim();
+        final String email = editTextEmail.getText().toString().trim();
+        final String country = editTextCountry.getText().toString().trim();
+        //Log.d(TAG, name);
+        //Log.d(TAG, phone);
         //Log.d(TAG, password);
+        //Log.d(TAG, passwordR);
+        //Log.d(TAG, email);
+        //Log.d(TAG, country);
 
         // If input is empty
-        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
-            Toast.makeText(this, "Please enter a valid email and password", Toast.LENGTH_SHORT).show();
+        if (TextUtils.isEmpty(name) || TextUtils.isEmpty(phone) || TextUtils.isEmpty(password) || TextUtils.isEmpty(passwordR) || TextUtils.isEmpty(email) || TextUtils.isEmpty(country)) {
+            Toast.makeText(this, "Please enter valid values!", Toast.LENGTH_LONG).show();
+            return;
+        } else if (!password.equals(passwordR)) {
+            Toast.makeText(this, "The passwords don't match!", Toast.LENGTH_LONG).show();
             return;
         } else {
             progressDialog.setMessage("Registering User...");
             progressDialog.show();
 
+            // Create a user on the Firebase Auth
             firebaseAuth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
@@ -66,15 +93,35 @@ public class Register extends AppCompatActivity {
 
                             if (task.isSuccessful()) {
                                 Log.d(TAG, "createUserWithEmail:success");
-                                progressDialog.cancel();
-                                Toast.makeText(Register.this, "Successfully registered User!", Toast.LENGTH_SHORT).show();
 
-                                // Redirect to login
-                                startActivity(new Intent(Register.this, Login.class));
+                                // Create new User with our own class
+                                User user = new User(name, phone, password, email, country); // TODO: Save the user ID from the Auth into the FireStore Database collection
+
+                                // Save User to Firestore
+                                firestore.collection("users").add(user)
+                                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                            @Override
+                                            public void onSuccess(DocumentReference documentReference) {
+                                                Log.d(TAG, "addUser:success");
+
+                                                progressDialog.cancel();
+                                                Toast.makeText(Register.this, "Successfully registered User!", Toast.LENGTH_SHORT).show();
+
+                                                // Redirect to login
+                                                startActivity(new Intent(Register.this, Login.class));
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception error) {
+                                                Log.d(TAG, "addUser:failure", error);
+                                                progressDialog.cancel();
+                                            }
+                                        });
                             } else {
                                 Log.w(TAG, "createUserWithEmail:failure", task.getException());
                                 progressDialog.cancel();
-                                Toast.makeText(Register.this, "Failed registering User! Check if you have Wifi turned on.", Toast.LENGTH_LONG).show();
+                                Toast.makeText(Register.this, "" + task.getException().getMessage(), Toast.LENGTH_LONG).show();
                             }
                         }
                     });
---
+++ app/src/main/java/com/sihbar/netz/User.java
@@ -0,0 +1,56 @@
+package com.sihbar.netz;
+
+public class User {
+
+    private String name, phone, password, email, country;
+
+    //public User() {};
+
+    public User(String name, String phone, String password, String email, String country) {
+        this.name = name;
+        this.phone = phone;
+        this.password = password;
+        this.email = email;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
