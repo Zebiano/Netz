122e9375dac202ebac9b05739670c5c149108b08
Zebiano <sebas1@hotmail.de>
2019-05-31 14:31:40 +0100

    Camera Works inside fragments

--- app/src/main/java/com/sihbar/netz/HomeFragment.java
+++ app/src/main/java/com/sihbar/netz/HomeFragment.java
@@ -24,6 +24,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.camerakit.CameraKitView;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -43,144 +44,48 @@ import java.util.List;
 import static android.app.Activity.RESULT_OK;
 
 public class HomeFragment extends Fragment{
-    //implements SurfaceHolder.Callback
 
     // Variables
     private static final String TAG = "HomeFragment";
-    public ProgressDialog progressDialog;
-    private static final int CAMERA_REQUEST = 1888;
-    Camera camera;
-    SurfaceView surfaceView;
-    SurfaceHolder surfaceHolder;
 
-    // Firebase
-    public FirebaseFirestore firestore;
-
-    // Machine-Learning
-    // High-accuracy landmark detection and face classification
-    FirebaseVisionFaceDetectorOptions highAccuracyOpts =
-            new FirebaseVisionFaceDetectorOptions.Builder()
-                    .setPerformanceMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
-                    .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
-                    .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
-                    .build();
-
-    // Real-time contour detection of multiple faces
-    FirebaseVisionFaceDetectorOptions realTimeOpts =
-            new FirebaseVisionFaceDetectorOptions.Builder()
-                    .setContourMode(FirebaseVisionFaceDetectorOptions.ALL_CONTOURS)
-                    .build();
-
-    // Custom Settings
-    FirebaseVisionFaceDetectorOptions customOpts =
-            new FirebaseVisionFaceDetectorOptions.Builder()
-                    .setPerformanceMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
-                    .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
-                    .enableTracking()
-                    .build();
+    // CameraKit
+    private CameraKitView cameraKitView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        Log.d(TAG, "onCreateView: ");
-
-        // Variables
-        /*View view = inflater.inflate(R.layout.fragment_home, container, false);
-
-        surfaceView = view.findViewById(R.id.surfaceView);
-        surfaceHolder = surfaceView.getHolder();
-
-        surfaceHolder.addCallback(this);
-        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);*/
-
-        openCamera();
-
         return inflater.inflate(R.layout.fragment_home, null);
     }
 
-    // Open Camera
-    public void openCamera() {
-        // TODO: Permissions for camera
-        /*if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
-        {
-            requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
-        }
-        else
-        {
-            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(cameraIntent, CAMERA_REQUEST);
-        }*/
-
-        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(cameraIntent, CAMERA_REQUEST);
-    }
-
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.d(TAG, "onActivityResult: ");
-        
-        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK)
-        {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-
-            FirebaseVisionImage mlImage = FirebaseVisionImage.fromBitmap(photo);
-
-            FirebaseVisionFaceDetector detector = FirebaseVision.getInstance()
-                    .getVisionFaceDetector(customOpts);
-
-            final Task<List<FirebaseVisionFace>> result =
-                    detector.detectInImage(mlImage)
-                            .addOnSuccessListener(
-                                    new OnSuccessListener<List<FirebaseVisionFace>>() {
-                                        @Override
-                                        public void onSuccess(List<FirebaseVisionFace> faces) {
-                                            // Task completed successfully
-                                            Log.d(TAG, "onSuccess: " + faces);
-                                        }
-                                    })
-                            .addOnFailureListener(
-                                    new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception e) {
-                                            // Task failed with an exception
-                                            Log.d(TAG, "onFailure: " + e);
-                                        }
-                                    });
-        }
-    }
-
-    /*@Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        camera = Camera.open();
-        Camera.Parameters parameters;
-        parameters = camera.getParameters();
-
-        camera.setDisplayOrientation(90);
-        parameters.setPreviewFrameRate(30);
-        parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
-
-        camera.setParameters(parameters);
-
-        try {
-            camera.setPreviewDisplay(surfaceHolder);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        camera.startPreview();
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        cameraKitView = view.findViewById(R.id.camera);
     }
 
     @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
+    public void onStart() {
+        super.onStart();
+        cameraKitView.onStart();
     }
-
     @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-
-    }*/
-
-    //
-    /*public void processImage() {
-
-    }*/
-
+    public void onResume() {
+        super.onResume();
+        cameraKitView.onResume();
+    }
+    @Override
+    public void onPause() {
+        cameraKitView.onPause();
+        super.onPause();
+    }
+    @Override
+    public void onStop() {
+        cameraKitView.onStop();
+        super.onStop();
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        cameraKitView.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 }
